a9d630bc7c5b6791f95247c9f8515ddcc8bf94d8
jithendrae <jithendra.edupuganti@imaginea.com>
2015-02-20 17:57:09 -0300

    Adding maven project to git local repo

---
+++ web-crawler/src/main/java/com/imaginea/training/jithendrae/web_crawler/WebCrawlerApp.java
@@ -0,0 +1,13 @@
+package com.imaginea.training.jithendrae.web_crawler;
+
+/**
+ * Web Crawler!
+ *
+ */
+public class WebCrawlerApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Web Crawler!" );
+    }
+}
---
+++ web-crawler/src/test/java/com/imaginea/training/jithendrae/web_crawler/WebCrawlerAppTest.java
@@ -0,0 +1,38 @@
+package com.imaginea.training.jithendrae.web_crawler;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class WebCrawlerAppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public WebCrawlerAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( WebCrawlerAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
